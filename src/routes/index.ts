1cdd095bca6f3659eea5d716143aa53e7eba14ee
cjnm <seejan.raj@live.com>
2023-02-23 15:17:30 +0545

    updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,30 @@
 import { Router } from 'express';
-// import fileRoutes from './fileRoutes';
-import collectionRoutes from './collectionRoutes';
-
 const router = Router();
 
-// router.use('/files', fileRoutes);
-router.use('/collections', collectionRoutes);
+router.get('/orders', (req, res, next) => {
+  const order = {
+    user: {
+      id: 123,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    },
+    orders: [
+      {
+        id: 456,
+        product: 'Widget A',
+        quantity: 2,
+        price: 10.99,
+      },
+      {
+        id: 789,
+        product: 'Widget B',
+        quantity: 1,
+        price: 19.99,
+      },
+    ],
+  };
+
+  return res.status(200).send(order);
+});
 
 export default router;
