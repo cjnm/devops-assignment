3321294fd26a158eabe062f72b7bc7119f1270f9
cjnm <seejan.raj@live.com>
2023-02-26 23:06:20 +0545

    updated

---
+++ frontend/src/About.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function About() {
+
+  return (
+    <div className="App">
+      <div>
+        <h1>About Page</h1>
+        <button><a href='/'>Goto Home</a></button>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Home Page</h1>
+        <button><a href='/about'>Goto About</a></button>
+      </div>
+    </div>
+  )
+}
+
+export default App
